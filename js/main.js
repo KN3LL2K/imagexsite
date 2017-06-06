309ba03df0def6797cd4be1e13ca4b7c9ac4a2d7
Harry Bellenie <harrybellenie@gmail.com>
2017-06-06 15:56:57 -0700

    init

---
+++ js/main.js
@@ -0,0 +1,21 @@
+(function() {
+  $(document).ready(function() {
+    var sidenav = $('#sidenav');
+    var navbarToggle = $('#navbar-toggle');
+    var block = $('.block');
+    var body = $('body');
+
+    // show mobile sidenav
+    navbarToggle.on('click', function() {
+      sidenav.css('right', 0);
+      block.css('display', 'inline');
+      body.css('overflow-y','hidden');
+    })
+    // close mobile sidenav
+    block.on('click', function() {
+      sidenav.css('right', '-200px');
+      block.css('display', 'none');
+      body.css('overflow-y','auto');
+    })
+  })
+})()
