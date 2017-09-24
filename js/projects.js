299293958d41f7fd254a7262beae4e50bfc41249
Harry Bellenie <harrybellenie@gmail.com>
2017-09-24 13:28:03 -0700

    adds project page, finishes site

---
+++ js/projects.js
@@ -0,0 +1,21 @@
+(function() {
+  $(document).ready(function() {
+
+    $(".owl-carousel").owlCarousel({
+      center: true,
+      items: 15,
+      nav: true,
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      autoplaySpeed: 450,
+      mouseDrag: false,
+      touchDrag: false,
+      dots: false,
+      autoWidth: true
+    });
+
+
+  })
+})()
+
